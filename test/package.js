b30754ad446ecdd92f67a76c77edacd0d2f38532
Martin M. <mcm57@gmx.at>
2026-02-16 21:23:40 +0100

    Re-enable package files validation test

--- test/package.js
+++ test/package.js
@@ -2,4 +2,4 @@ const path = require('path');
 const { tests } = require('@iobroker/testing');
 
 // Validate the package files
-// ## to be reenabled ## tests.packageFiles(path.join(__dirname, '..'));
+tests.packageFiles(path.join(__dirname, '..'));
